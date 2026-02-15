77a0e3776125d09efc2ee527a2e88a25187d5d75
pjsangat@gmail.com <pjsangat@gmail.com>
2026-02-15 11:33:26 +0800

    updates

--- app/Views/admin/includes/layout.php
+++ app/Views/admin/includes/layout.php
@@ -65,11 +65,11 @@
     <!-- ./wrapper -->
 
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous" ></script>
-    <script src="http://handglove.local/assets/dist/js/plugins/popper.min.js" ></script>
-    <script src="http://handglove.local/assets/dist/js/plugins/bootstrap-4.5.2/bootstrap.min.js" ></script>
-    <script src="http://handglove.local/assets/dist/js/components/global.min.js" ></script>
-    <script src="http://handglove.local/assets/dist/js/plugins/toastr.min.js" ></script>
-    <script src="http://handglove.local/assets/dist/js/components/navigation_bar.min.js" ></script>
+    <script src="/assets/dist/js/plugins/popper.min.js" ></script>
+    <script src="/assets/dist/js/plugins/bootstrap-4.5.2/bootstrap.min.js" ></script>
+    <script src="/assets/dist/js/components/global.min.js" ></script>
+    <script src="/assets/dist/js/plugins/toastr.min.js" ></script>
+    <script src="/assets/dist/js/components/navigation_bar.min.js" ></script>
 
     <?php 
     if(isset($scripts) && !empty($scripts) && is_array($scripts)){
